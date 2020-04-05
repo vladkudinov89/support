af3cca519c31b56f6e0dd62ea42fc472c429117a
vladkudino89 <vlad.kudinov.89@gmail.com>
2020-04-05 09:26:21 +0300

    Add redirect feature to login page. Add test. test ok.

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+
+Route::group(['middleware' => 'auth'], function () {
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+});
+
---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function unauth_user_cannot_see_homepage()
+    {
+       $response = $this->get('/');
+
+        $response->assertRedirect('/login');
+    }
+
+    /** @test */
+    public function auth_user_can_see_homepage()
+    {
+        $user = factory(User::class)->create();
+
+        $response =
+            $this->actingAs($user)
+                ->get('/')
+                ->assertSuccessful();
+    }
+
+
+}
